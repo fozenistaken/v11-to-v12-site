57979485befe61a6495721889e6b8a1d454d079c
Glitch (vivid-deserted-mandible) <none>
2020-11-29 14:00:51 +0000

    🌑📻 Checkpoint ./index.js:23431879/150 ./package.json:23431879/2074 ./form.ejs:23431879/482 ./css/main.css:23431879/46

--- README.md
+++
@@ -1,2 +0,0 @@
-# Name? Develoment FormSite
-Template By Colorlib
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ app.use(express.static("public"));
 const request = require("request");
 const db = require("quick.db");
 const fs = require("fs");
+const n = require("nodme")
 const url = require("url");
 const path = require("path");
 var bodyParser = require('body-parser')
@@ -32,11 +33,11 @@ app.use(bodyParser.json())
     );
   };
   app.get("/", (req, res) => {
-    if(!req.query.email || !req.query.dcname || !req.query.lastname || !req.query.name || !req.query.message || !req.query.fullname) {
+    if(/*!req.query.email || !req.query.dcname || !req.query.lastname || !req.query.name || */!req.query.message) {
       //  {"fullname":"Name? Dev","email":"namedev@gmail.com","dcname":"cenap#0001","lastname":"yuce","name":"11","message":"test"}
       renderTemplate(res, req, "form.ejs"); 
   } else {
-     const hook = new Discord.WebhookClient('WEBHOOK ID', 'WEBHOOK TOKEN');
+    /* const hook = new Discord.WebhookClient('WEBHOOK ID', 'WEBHOOK TOKEN');
 const webhook = new Discord.MessageEmbed()
 .setTitle("Başvuru")
 .setColor("GREEN")
@@ -47,8 +48,9 @@ const webhook = new Discord.MessageEmbed()
 .addField('Alanı: ', req.query.name , true)
 .addField('\u200b', '\u200b')
 .addField('Neden Sen: ', req.query.message , true)
-hook.send(webhook)
-    res.send("Başvurunuz Gönderildi!")
+hook.send(webhook)*/
+    var newCode = n.replace(req.query.message)
+    res.send(newCode)
 
 }
     
