7790de9e8e1d98e28483c70a12c82a7c8d3375b7
Glitch (vivid-deserted-mandible) <none>
2020-12-04 12:27:47 +0000

    🚨😅 Checkpoint ./index.js:21197947/81:23431879/9193 ./fozen.ejs:23431879/143

--- index.js
+++ index.js
@@ -4,48 +4,117 @@ app.use(express.static("public"));
 const request = require("request");
 const db = require("quick.db");
 const fs = require("fs");
-const haste = require("hastebin-gen")
-const n = require("nodme")
+const haste = require("hastebin-gen");
+const n = require("nodme");
 const url = require("url");
 const path = require("path");
-var bodyParser = require('body-parser')
-const Discord = require("discord.js")
-  app.use(
-    "/css",
-    express.static(path.resolve(__dirname + `/css`))
-  );
+var bodyParser = require("body-parser");
+const Discord = require("discord.js");
+app.use("/css", express.static(path.resolve(__dirname + `/css`)));
 
-    const templateDir = path.resolve(__dirname + `/`);  // SITE DOSYA KONTROL
+const templateDir = path.resolve(__dirname + `/`); // SITE DOSYA KONTROL
 
-  app.locals.domain = process.env.PROJECT_DOMAIN;
-  app.engine("html", require("ejs").renderFile);
-  app.set("view engine", "html");
+app.locals.domain = process.env.PROJECT_DOMAIN;
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
-  var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-  const renderTemplate = (res, req, template, data = {}) => {
-    const baseData = {
-      path: req.path
-    };
-    res.render(
-      path.resolve(`${templateDir}${path.sep}${template}`),
-      Object.assign(baseData, data)
-    );
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const renderTemplate = (res, req, template, data = {}) => {
+  const baseData = {
+    path: req.path
   };
-  app.get("/", (req, res) => {
-    if(!req.query.message) {
-      renderTemplate(res, req, "fozen.ejs"); 
+  res.render(
+    path.resolve(`${templateDir}${path.sep}${template}`),
+    Object.assign(baseData, data)
+  );
+};
+app.get("/", (req, res) => {
+  if (!req.query.message) {
+    renderTemplate(res, req, "fozen.ejs");
   } else {
-    var newCode = n.replace(req.query.message)//ya split joi
-    var asıl = newCode.split("client.ping").join("client.ws.ping")//modül var ne olur ne olmaz senin kısımıda koyalım
-    haste(asıl, { extension: "Yusuf&Fozen" }).then(x =>//başka bişi o MAL
-    res.redirect(x))
-
-}
-    
-  });
-
+    var newCode = n.replace(req.query.message);
+    var asıl = newCode
+      .split("client.ping")
+      .join("client.ws.ping")
+      .split("get")
+      .join("cache.get")
+      .split("addRole")
+      .join("roles.add")
+      .split("removeRole")
+      .join("roles.remove")
+      .split("users.exists")
+      .join("users.cache.some")
+      .split("channels.exists")
+      .join("channels.cache.some")
+      .split("find")
+      .join("cache.find")
+      .split("RichEmbed")
+      .join("MessageEmbed")
+      .split("fetchUser")
+      .join("users.fetch")
+      .split("fetchMember")
+      .join("users.members")
+      .split("fetchMessage")
+      .join("users.messages")
+      .split("fetchPinnedMessages")
+      .join("messages.fetchPinned")
+      .split("sendMessage")
+      .join("send")
+      .split("sendEmbed")
+      .join("send")
+      .split("sendCode")
+      .join("send")
+      .split("sendFile")
+      .join("send")
+      .split("sendFiles")
+      .join("send")
+      .split("setRoles")
+      .join("roles.set")
+      .split("colorRole")
+      .join("roles.color")
+      .split("highestRole")
+      .join("roles.highest")
+      .split("hoistRole")
+      .join("roles.hoist")
+      .split("ban")
+      .join("members.ban")
+      .split("unban")
+      .join("members.unban")
+      .split("avatarURL")
+      .join("avatarURL()")
+      .split("displayAvatarURL")
+      .join("displayAvatarURL()")
+      .split("iconURL")
+      .join("iconURL()")
+      .split("splashURL")
+      .join("splashURL()")
+      .split("playFile")
+      .join("play")
+      .split("playStream")
+      .join("play")
+      .split("playArbitraryInput")
+      .join("play")
+      .split("playBroadcast")
+      .join("play")
+      .split("playOpusStream")
+      .join("play")
+      .split("playConvertedStream")
+      .join("play")
+      .split("dispatcher.end()")
+      .join("dispatcher.destroy()")
+      .split("createVoiceBroadcast")
+      .join("voice.createBroadcast")
+      .split("broadcast.dispatchers")
+      .join("broadcast.subscribers")
+      .split("forEach")
+      .join("cache.forEach");
+    haste(asıl, { extension: "Yusuf&Fozen" }).then(x => res.redirect(x));
+    db.add(`bnmç_${req.user.id}`, 1)
+    db.add(`topç`, 1)
+  }
+});
 
 const listener = app.listen(process.env.PORT, () => {
   console.log("Panel şu portla başlatıldı:" + listener.address().port);
